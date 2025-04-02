8353805831850acf447da1f626ad7ac0ccc1a368
하경 <hamhagyeong@hakyeong-ui-MacBookAir.local>
2025-04-02 23:48:14 +0900

    4주차 과제 업로드

---
+++ week04/todolist/src/components/TodoList.jsx
@@ -0,0 +1,20 @@
+import TodoItem from "./TodoItem";
+
+function TodoList({todos, onRemove, onToggle, onUpdate}) {
+    return <div>
+        {todos.map(todo => (  //todo 배열의 각 todo 객체로 
+            <TodoItem         //TodoItem 컴포넌트를 하나씩 만든다 
+            key={todo.id}
+            id={todo.id}
+            text={todo.text}
+            done={todo.done}  
+            onRemove={onRemove}
+            onToggle={onToggle}
+            onUpdate={onUpdate}
+            />
+        ))}
+    </div>;
+  }
+  
+
+export default TodoList;
