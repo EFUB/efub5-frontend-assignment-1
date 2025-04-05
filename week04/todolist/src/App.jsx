31001e9dadac946713f7ea91647c23ebf19481e3
pororori <cathy0e0711@gmail.com>
2025-04-05 18:08:46 +0900

    feat: App.jsx 코드 추가

--- week04/todolist/src/App.jsx
+++ week04/todolist/src/App.jsx
@@ -1,5 +1,4 @@
-//App.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { createGlobalStyle } from "styled-components";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoHead from "./components/TodoHead";
@@ -11,30 +10,29 @@ const GlobalStyle = createGlobalStyle`
     background: #e9ecef
   }
 `;
-const TODO_LIST=[
-  {id:1,
-    text:"투두리스트",
-    done:false},
-  {
-    id:2,
-    text:"이펍 가기",
-    done:false,
-  },
-];
 
 function App() {
+  const [todos, setTodos] = useState(() => {
+    const storedTodos = localStorage.getItem('todos');
+    return storedTodos ? JSON.parse(storedTodos) : [
+      //{ id: 1, text: "투두리스트", done: false },
+      //{ id: 2, text: "이펍 가기", done: false },
+    ];
+  });
 
-  const [todos,setTodos]=useState(TODO_LIST);
-
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <>
-      <GlobalStyle />
-      <TodoTemplate>
-        <TodoHead todos={todos}/>
-        <TodoList todos={todos} setTodos={setTodos}/>
-        <TodoCreate todos={todos} setTodos={setTodos}/>
-      </TodoTemplate>
+      
+        <TodoHead todos={todos} /> {/* TodoHead 컴포넌트 사용 [3] */}
+        
+          <TodoList todos={todos} setTodos={setTodos} /> {/* TodoList 컴포넌트 사용 [3, 9] */}
+          <TodoCreate todos={todos} setTodos={setTodos} /> {/* TodoCreate 컴포넌트 사용 [1, 3] */}
+        
+      
     </>
   );
 }
